04a5501fb29d0b5bb3423102305157bc9daa4428
Kot <kot@yukata.tech>
2022-05-14 21:18:20 -0700

    Prepare to handle ranges and etags

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import parseRange from "range-parser"
+
 interface Env {
   R2_BUCKET: R2Bucket
 }
@@ -22,19 +24,50 @@ export default {
     if (!response || !response.ok) {
       const path = url.pathname.substring(1);
 
+      // Range handling (Currently bugged in R2, so disabled)
+      // let head: R2Object | null | undefined;
+      // let range: R2Range | undefined;
+      // if (request.method === "GET") {
+      //   const rangeHeader = request.headers.get("range");
+      //   if (rangeHeader) {
+      //     head = await env.R2_BUCKET.head(path);
+      //     const parsedRanges = parseRange(head?.size || 0, rangeHeader);
+      //     if (parsedRanges !== -1 && parsedRanges !== -2 && parsedRanges.length === 1) {
+      //       let firstRange = parsedRanges[0];
+      //       range = {
+      //         offset: firstRange.start,
+      //         length: firstRange.end - firstRange.start
+      //       }
+      //       console.log(range);
+      //     } else {
+      //       return new Response("Range Not Satisfiable", { status: 416 });
+      //     }
+      //   }
+      // }
+
+      // Etag/If-(Not)-Match handling
+      // R2 requires that etag checks must not contain quotes
+      // const processMatchHeaders = (header: string | null) => header?.split(",")
+      //   .map(item => item.startsWith("W/") ? null : item.trim().replace(/^['"]|['"]$/g, "")).filter(Boolean).join(",");
+      //
+      // const ifMatch = processMatchHeaders(request.headers.get("if-match"));
+      // const ifNoneMatch = processMatchHeaders(request.headers.get("if-none-match"));
+
+      // const options = { onlyIf: { etagMatches: ifMatch || undefined, etagDoesNotMatch: ifNoneMatch || undefined }, range };
+
       const file = request.method === "HEAD" ? await env.R2_BUCKET.head(path) : await env.R2_BUCKET.get(path);
       if (file === null) {
         return new Response("File Not Found", { status: 404 });
       }
 
       function hasBody(object: R2Object | R2ObjectBody): object is R2ObjectBody {
-        return (file?.size || 0) !== 0 && (<R2ObjectBody>object).body !== undefined;
+        return (<R2ObjectBody>object).body !== undefined;
       }
 
       const shouldSendBody = hasBody(file);
 
       response = new Response(shouldSendBody ? file.body : null, {
-        status: shouldSendBody ? 200 : 204,
+        status: (file?.size || 0) === 0 ? 204 : 200,
         headers: {
           "etag": file.httpEtag,
           "cache-control": file.httpMetadata.cacheControl ?? "",
@@ -49,7 +82,8 @@ export default {
       });
     }
 
-    ctx.waitUntil(cache.put(request, response.clone()));
+    if (request.method === "GET")
+      ctx.waitUntil(cache.put(request, response.clone()));
     return response;
   },
 };
