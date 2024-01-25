026841d4e08c878faf9734917e41a664a1b15d5a
Kot <kot@yukata.dev>
2024-01-25 10:10:04 -0600

    Add pagination to listings (1.4.0)

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 import parseRange from "range-parser";
 
 export interface Env {
-  R2_BUCKET: R2Bucket,
-  ALLOWED_ORIGINS?: string,
-  CACHE_CONTROL?: string,
-  PATH_PREFIX?: string
-  INDEX_FILE?: string
-  NOTFOUND_FILE?: string
-  DIRECTORY_LISTING?: boolean
-  HIDE_HIDDEN_FILES?: boolean
-  DIRECTORY_CACHE_CONTROL?: string
-  LOGGING?: boolean
+  R2_BUCKET: R2Bucket;
+  ALLOWED_ORIGINS?: string;
+  CACHE_CONTROL?: string;
+  PATH_PREFIX?: string;
+  INDEX_FILE?: string;
+  NOTFOUND_FILE?: string;
+  DIRECTORY_LISTING?: boolean;
+  ITEMS_PER_PAGE?: number;
+  HIDE_HIDDEN_FILES?: boolean;
+  DIRECTORY_CACHE_CONTROL?: string;
+  LOGGING?: boolean;
 }
 
-const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+const units = ["B", "KB", "MB", "GB", "TB"];
 
-type ParsedRange = { offset: number, length: number } | { suffix: number };
+type ParsedRange = { offset: number; length: number } | { suffix: number };
 
-function rangeHasLength(object: ParsedRange): object is { offset: number, length: number } {
-  return (<{ offset: number, length: number }>object).length !== undefined;
+function rangeHasLength(
+  object: ParsedRange
+): object is { offset: number; length: number } {
+  return (<{ offset: number; length: number }>object).length !== undefined;
 }
 
 function hasBody(object: R2Object | R2ObjectBody): object is R2ObjectBody {
@@ -30,19 +33,29 @@ function hasSuffix(range: ParsedRange): range is { suffix: number } {
 }
 
 function getRangeHeader(range: ParsedRange, fileSize: number): string {
-  return `bytes ${hasSuffix(range) ? (fileSize - range.suffix) : range.offset}-${hasSuffix(range) ? fileSize - 1 :
-    (range.offset + range.length - 1)}/${fileSize}`;
+  return `bytes ${hasSuffix(range) ? fileSize - range.suffix : range.offset}-${
+    hasSuffix(range) ? fileSize - 1 : range.offset + range.length - 1
+  }/${fileSize}`;
 }
 
 // some ideas for this were taken from / inspired by
 // https://github.com/cloudflare/workerd/blob/main/samples/static-files-from-disk/static.js
-async function makeListingResponse(path: string, env: Env, request: Request): Promise<Response | null> {
-  if (path === "/")
-    path = ""
+async function makeListingResponse(
+  path: string,
+  env: Env,
+  request: Request
+): Promise<Response | null> {
+  if (path === "/") path = "";
   else if (path !== "" && !path.endsWith("/")) {
     path += "/";
   }
-  let listing = await env.R2_BUCKET.list({ prefix: path, delimiter: '/' })
+  let cursor = new URL(request.url).searchParams.get("cursor") || undefined;
+  let listing = await env.R2_BUCKET.list({
+    prefix: path,
+    delimiter: "/",
+    cursor,
+    limit: env.ITEMS_PER_PAGE || 1000,
+  });
 
   if (listing.delimitedPrefixes.length === 0 && listing.objects.length === 0) {
     return null;
@@ -57,36 +70,46 @@ async function makeListingResponse(path: string, env: Env, request: Request): Pr
     if (path !== "") {
       htmlList.push(
         `      <tr>` +
-        `<td><a href="../">../</a></td>` +
-        `<td>-</td><td>-</td></tr>`);
+          `<td><a href="../">../</a></td>` +
+          `<td>-</td><td>-</td></tr>`
+      );
     }
 
     for (let dir of listing.delimitedPrefixes) {
-      if (dir.endsWith("/")) dir = dir.substring(0, dir.length - 1)
-      let name = dir.substring(path.length, dir.length)
+      if (dir.endsWith("/")) dir = dir.substring(0, dir.length - 1);
+      let name = dir.substring(path.length, dir.length);
       if (name.startsWith(".") && env.HIDE_HIDDEN_FILES) continue;
       htmlList.push(
         `      <tr>` +
-        `<td><a href="${encodeURIComponent(name)}/">${name}/</a></td>` +
-        `<td>-</td><td>-</td></tr>`);
+          `<td><a href="${encodeURIComponent(name)}/">${name}/</a></td>` +
+          `<td>-</td><td>-</td></tr>`
+      );
     }
     for (let file of listing.objects) {
-      let name = file.key.substring(path.length, file.key.length)
+      let name = file.key.substring(path.length, file.key.length);
       if (name.startsWith(".") && env.HIDE_HIDDEN_FILES) continue;
 
-      let dateStr = file.uploaded.toISOString()
-      dateStr = dateStr.split('.')[0].replace('T', ' ')
-      dateStr = dateStr.slice(0, dateStr.lastIndexOf(':')) + 'Z'
+      let dateStr = file.uploaded.toISOString();
+      dateStr = dateStr.split(".")[0].replace("T", " ");
+      dateStr = dateStr.slice(0, dateStr.lastIndexOf(":")) + "Z";
 
       htmlList.push(
         `      <tr>` +
-        `<td><a href="${encodeURIComponent(name)}">${name}</a></td>` +
-        `<td>${dateStr}</td><td>${niceBytes(file.size)}</td></tr>`);
+          `<td><a href="${encodeURIComponent(name)}">${name}</a></td>` +
+          `<td>${dateStr}</td><td>${niceBytes(file.size)}</td></tr>`
+      );
 
       if (lastModified == null || file.uploaded > lastModified) {
         lastModified = file.uploaded;
       }
+    }
 
+    if (listing.truncated) {
+      htmlList.push(
+        `      <tr>` +
+          `<td><a href="?cursor=${listing.cursor}">...see more.../</a></td>` +
+          `<td>-</td><td>-</td></tr>`
+      );
     }
 
     if (path === "") path = "/";
@@ -124,8 +147,8 @@ ${htmlList.join("\n")}
     </table>
   </body>
 </html>
-  `
-  };
+  `;
+  }
 
   return new Response(html === "" ? null : html, {
     status: 200,
@@ -133,27 +156,36 @@ ${htmlList.join("\n")}
       "access-control-allow-origin": env.ALLOWED_ORIGINS || "",
       "last-modified": lastModified === null ? "" : lastModified.toUTCString(),
       "content-type": "text/html",
-      "cache-control": env.DIRECTORY_CACHE_CONTROL || "no-store"
-    }
+      "cache-control": env.DIRECTORY_CACHE_CONTROL || "no-store",
+    },
   });
 }
 
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
     const allowedMethods = ["GET", "HEAD", "OPTIONS"];
     if (allowedMethods.indexOf(request.method) === -1) {
-      return new Response("Method Not Allowed", { status: 405, headers: { "allow": allowedMethods.join(", ") } });
+      return new Response("Method Not Allowed", {
+        status: 405,
+        headers: { allow: allowedMethods.join(", ") },
+      });
     }
 
     if (request.method === "OPTIONS") {
-      return new Response(null, { headers: { "allow": allowedMethods.join(", ") } })
+      return new Response(null, {
+        headers: { allow: allowedMethods.join(", ") },
+      });
     }
 
     let triedIndex = false;
 
     let response: Response | undefined;
 
-    const isCachingEnabled = env.CACHE_CONTROL !== "no-store"
+    const isCachingEnabled = env.CACHE_CONTROL !== "no-store";
     const cache = caches.default;
     if (isCachingEnabled) {
       response = await cache.match(request);
@@ -199,15 +231,24 @@ export default {
         const rangeHeader = request.headers.get("range");
         if (rangeHeader) {
           file = await env.R2_BUCKET.head(path);
-          if (file === null) return new Response("File Not Found", { status: 404 });
+          if (file === null)
+            return new Response("File Not Found", { status: 404 });
           const parsedRanges = parseRange(file.size, rangeHeader);
           // R2 only supports 1 range at the moment, reject if there is more than one
-          if (parsedRanges !== -1 && parsedRanges !== -2 && parsedRanges.length === 1 && parsedRanges.type === "bytes") {
+          if (
+            parsedRanges !== -1 &&
+            parsedRanges !== -2 &&
+            parsedRanges.length === 1 &&
+            parsedRanges.type === "bytes"
+          ) {
             let firstRange = parsedRanges[0];
-            range = file.size === (firstRange.end + 1) ? { suffix: file.size - firstRange.start } : {
-              offset: firstRange.start,
-              length: firstRange.end - firstRange.start + 1
-            };
+            range =
+              file.size === firstRange.end + 1
+                ? { suffix: file.size - firstRange.start }
+                : {
+                    offset: firstRange.start,
+                    length: firstRange.end - firstRange.start + 1,
+                  };
           } else {
             return new Response("Range Not Satisfiable", { status: 416 });
           }
@@ -217,12 +258,17 @@ export default {
       // Etag/If-(Not)-Match handling
       // R2 requires that etag checks must not contain quotes, and the S3 spec only allows one etag
       // This silently ignores invalid or weak (W/) headers
-      const getHeaderEtag = (header: string | null) => header?.trim().replace(/^['"]|['"]$/g, "");
+      const getHeaderEtag = (header: string | null) =>
+        header?.trim().replace(/^['"]|['"]$/g, "");
       const ifMatch = getHeaderEtag(request.headers.get("if-match"));
       const ifNoneMatch = getHeaderEtag(request.headers.get("if-none-match"));
 
-      const ifModifiedSince = Date.parse(request.headers.get("if-modified-since") || "");
-      const ifUnmodifiedSince = Date.parse(request.headers.get("if-unmodified-since") || "");
+      const ifModifiedSince = Date.parse(
+        request.headers.get("if-modified-since") || ""
+      );
+      const ifUnmodifiedSince = Date.parse(
+        request.headers.get("if-unmodified-since") || ""
+      );
 
       const ifRange = request.headers.get("if-range");
       if (range && ifRange && file) {
@@ -230,7 +276,8 @@ export default {
 
         if (isNaN(maybeDate) || new Date(maybeDate) > file.uploaded) {
           // httpEtag already has quotes, no need to use getHeaderEtag
-          if (ifRange.startsWith("W/") || ifRange !== file.httpEtag) range = undefined;
+          if (ifRange.startsWith("W/") || ifRange !== file.httpEtag)
+            range = undefined;
         }
       }
 
@@ -238,8 +285,11 @@ export default {
         file = await env.R2_BUCKET.get(path, {
           onlyIf: {
             etagMatches: ifMatch,
-            uploadedBefore: ifUnmodifiedSince ? new Date(ifUnmodifiedSince) : undefined
-          }, range
+            uploadedBefore: ifUnmodifiedSince
+              ? new Date(ifUnmodifiedSince)
+              : undefined,
+          },
+          range,
         });
 
         if (file && !hasBody(file)) {
@@ -250,25 +300,34 @@ export default {
       if (ifNoneMatch || ifModifiedSince) {
         // if-none-match overrides if-modified-since completely
         if (ifNoneMatch) {
-          file = await env.R2_BUCKET.get(path, { onlyIf: { etagDoesNotMatch: ifNoneMatch }, range });
+          file = await env.R2_BUCKET.get(path, {
+            onlyIf: { etagDoesNotMatch: ifNoneMatch },
+            range,
+          });
         } else if (ifModifiedSince) {
-          file = await env.R2_BUCKET.get(path, { onlyIf: { uploadedAfter: new Date(ifModifiedSince) }, range });
+          file = await env.R2_BUCKET.get(path, {
+            onlyIf: { uploadedAfter: new Date(ifModifiedSince) },
+            range,
+          });
         }
         if (file && !hasBody(file)) {
           return new Response(null, { status: 304 });
         }
       }
 
-      file = request.method === "HEAD"
-        ? await env.R2_BUCKET.head(path)
-        : ((file && hasBody(file)) ? file : await env.R2_BUCKET.get(path, { range }));
+      file =
+        request.method === "HEAD"
+          ? await env.R2_BUCKET.head(path)
+          : file && hasBody(file)
+          ? file
+          : await env.R2_BUCKET.get(path, { range });
 
       let notFound: boolean = false;
 
       if (file === null) {
         if (env.INDEX_FILE && triedIndex) {
           // remove the index file since it doesnt exist
-          path = path.substring(0, path.length - env.INDEX_FILE.length)
+          path = path.substring(0, path.length - env.INDEX_FILE.length);
         }
 
         if (env.DIRECTORY_LISTING && (path.endsWith("/") || path === "")) {
@@ -286,9 +345,10 @@ export default {
         if (env.NOTFOUND_FILE && env.NOTFOUND_FILE != "") {
           notFound = true;
           path = env.NOTFOUND_FILE;
-          file = request.method === "HEAD"
-            ? await env.R2_BUCKET.head(path)
-            : await env.R2_BUCKET.get(path);
+          file =
+            request.method === "HEAD"
+              ? await env.R2_BUCKET.head(path)
+              : await env.R2_BUCKET.get(path);
         }
 
         // if its still null, either 404 is disabled or that file wasn't found either
@@ -298,26 +358,38 @@ export default {
         }
       }
 
-      response = new Response((hasBody(file) && file.size !== 0) ? file.body : null, {
-        status: notFound ? 404 : (range ? 206 : 200),
-        headers: {
-          "accept-ranges": "bytes",
-          "access-control-allow-origin": env.ALLOWED_ORIGINS || "",
+      response = new Response(
+        hasBody(file) && file.size !== 0 ? file.body : null,
+        {
+          status: notFound ? 404 : range ? 206 : 200,
+          headers: {
+            "accept-ranges": "bytes",
+            "access-control-allow-origin": env.ALLOWED_ORIGINS || "",
 
-          "etag": notFound ? "" : file.httpEtag,
-          // if the 404 file has a custom cache control, we respect it
-          "cache-control": file.httpMetadata?.cacheControl ?? (notFound ? "" : env.CACHE_CONTROL || ""),
-          "expires": file.httpMetadata?.cacheExpiry?.toUTCString() ?? "",
-          "last-modified": notFound ? "" : file.uploaded.toUTCString(),
+            etag: notFound ? "" : file.httpEtag,
+            // if the 404 file has a custom cache control, we respect it
+            "cache-control":
+              file.httpMetadata?.cacheControl ??
+              (notFound ? "" : env.CACHE_CONTROL || ""),
+            expires: file.httpMetadata?.cacheExpiry?.toUTCString() ?? "",
+            "last-modified": notFound ? "" : file.uploaded.toUTCString(),
 
-          "content-encoding": file.httpMetadata?.contentEncoding ?? "",
-          "content-type": file.httpMetadata?.contentType ?? "application/octet-stream",
-          "content-language": file.httpMetadata?.contentLanguage ?? "",
-          "content-disposition": file.httpMetadata?.contentDisposition ?? "",
-          "content-range": (range && !notFound ? getRangeHeader(range, file.size) : ""),
-          "content-length": (range && !notFound ? (rangeHasLength(range) ? range.length : range.suffix) : file.size).toString()
+            "content-encoding": file.httpMetadata?.contentEncoding ?? "",
+            "content-type":
+              file.httpMetadata?.contentType ?? "application/octet-stream",
+            "content-language": file.httpMetadata?.contentLanguage ?? "",
+            "content-disposition": file.httpMetadata?.contentDisposition ?? "",
+            "content-range":
+              range && !notFound ? getRangeHeader(range, file.size) : "",
+            "content-length": (range && !notFound
+              ? rangeHasLength(range)
+                ? range.length
+                : range.suffix
+              : file.size
+            ).toString(),
+          },
         }
-      });
+      );
 
       if (request.method === "GET" && !range && isCachingEnabled && !notFound)
         ctx.waitUntil(cache.put(request, response.clone()));
@@ -332,12 +404,12 @@ export default {
 };
 
 function niceBytes(x: number) {
-
-  let l = 0, n = parseInt(x.toString(), 10) || 0;
+  let l = 0,
+    n = parseInt(x.toString(), 10) || 0;
 
   while (n >= 1000 && ++l) {
     n = n / 1000;
   }
 
-  return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
+  return n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l];
 }
